70b210e7a6388e774efc73e716447e85d7764173
fancy0815 <48304348+fancy0815@users.noreply.github.com>
2019-04-29 16:39:22 -0400

    Update and rename 2_Problems26-.sql to 2_Aggregation.sql

--- SQL/Hackerrank/2_Aggregation.sql
+++ SQL/Hackerrank/2_Aggregation.sql
@@ -17,7 +17,7 @@ WHERE district = 'California'
 SELECT FLOOR(AVG(population))
 FROM city
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#30
+#30 Japan Population
 SELECT SUM(population)
 FROM city
 WHERE COUNTRYCODE = 'JPN'
@@ -26,4 +26,41 @@ WHERE COUNTRYCODE = 'JPN'
 SELECT MAX(population)-MIN(population)
 FROM city
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#32 
+#32 The Blunder
+SELECT CEIL(SUM(
+             (salary-CAST(REPLACE(CAST(salary AS CHAR),'0','') AS SIGNED))
+            )/COUNT(*))
+FROM employees
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#33 Top Earners
+SELECT months*salary, count(*)
+FROM employee
+GROUP BY months*salary
+ORDER BY months*salary DESC
+LIMIT 1
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#34 Weather Observation Station 2
+SELECT ROUND(SUM(lat_n),2), ROUND(SUM(long_w),2)
+FROM station
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#35 Weather Observation Station 13
+SELECT ROUND(SUM(lat_n),4)
+FROM station
+WHERE lat_n > 38.7880 AND lat_n < 137.2345
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#36 Weather Observation Station 14
+SELECT ROUND(lat_n,4)
+FROM station
+WHERE lat_n < 137.2345
+ORDER BY lat_n DESC
+LIMIT 1
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
